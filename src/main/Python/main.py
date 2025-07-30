bc80b072687efc2ffa124ff7d9ecfdff9c2ce455
BluejayDev1 <senseibluejay@gmail.com>
2025-07-29 20:49:24 -0400

    Added event handlers. Closes #1

--- src/main/Python/main.py
+++ src/main/Python/main.py
@@ -19,3 +19,34 @@ intents.members = True
 
 # Set up the command prefix
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+# -------- Set up event handlers --------
+
+# Status: Ready
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('Panpakapan~! (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧')
+
+# Handler: New member joins (Send DM)
+@bot.event
+async def on_member_join(member):
+    await member.send(f'Welcome to the server, {member.name}! A new member has joined Arisu\'s Party!')
+
+# Handler: handle excluded words or certain phrases
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    # Profanity and racial slur filter set (Will be updated)
+    banned_words = {"nigger", "nigga", "rape", "raped", "beaner"}
+
+    if any(banned_word in message.content.lower() for banned_word in banned_words):
+        await message.delete()
+        await message.channel.send(f"{message.author.mention} is not allowed to say that here!")
+
+    await bot.process_commands(message)
+
+# Run the bot with token and log file handler
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
